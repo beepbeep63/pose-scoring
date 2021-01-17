78dd48d9b9c26cf7f022cecf7b2c6af5c5280341
Rabab <65520952+beepbeep63@users.noreply.github.com>
2021-01-16 21:35:33 -0500

    Test function and organizing stuff

---
+++ src/Real-Time-Human-Pose-detection-in-browser/poseNet-video/receive-videos.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const config = require('./config.json');
+
+
+ (async function() {
+   try {
+
+    AWS.config.setPromisesDependency();
+    AWS.config.update(
+      {
+        accessKeyId: "AKIAJGHNRH65ZOIUHWEA",
+        secretAccessKey: "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",
+        region: 'ca-central-1'
+      });
+
+    const s3 = new AWS.S3();
+    const response = await s3.listObjectsV2({
+      Bucket: 'htn2021'
+    }).promise();
+
+
+   } catch (e){}
+
+ })();
+// var s3 = new AWS.S3();
+// s3.getObject(
+//   { Bucket: "my-bucket", Key: "vid1" },
+//   function (error, data) {
+//     if (error != null) {
+//       alert("Failed to retrieve an object: " + error);
+//     } else {
+//       alert("Loaded " + data.ContentLength + " bytes");
+//       // do something with data.Body
+//     }
+//   }
+// );
