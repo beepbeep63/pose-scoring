e540c8f69c78bd251a94798528ad8ebafb940307
Rabab <65520952+beepbeep63@users.noreply.github.com>
2021-01-17 09:54:24 -0500

    final

--- src/Real-Time-Human-Pose-detection-in-browser/poseNet-video/poseNet_video2.js
+++ src/Real-Time-Human-Pose-detection-in-browser/poseNet-video/poseNet_video2.js
@@ -10,13 +10,6 @@
     P5.js: for drawing and creating video output in the browser.
 */
 
-import { danceScore } from './scoring.js';
-import { create, all } from 'mathjs';
-
-const similarity = require('compute-cosine-similarity');
-const config = { };
-const math = create(all, config);
-
 // ============= IMPORTANT ==============================
 // BELOW ARE THE VARIABLES TO CHANGE BASED ON YOUR VIDEO
 let canvas_height = 1080
